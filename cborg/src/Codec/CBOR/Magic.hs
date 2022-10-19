c93914589c0aa5def6fa4c44d9e14fea20828def
Alberto Valverde <alberto@toscat.net>
2022-10-19 19:44:06 +0200

    fix for 9.2.4

--- cborg/src/Codec/CBOR/Magic.hs
+++ cborg/src/Codec/CBOR/Magic.hs
@@ -125,6 +125,8 @@ import           GHC.Exts as Compat (wordToWord64#, word64ToWord#,
                                      intToInt64#, int64ToInt#,
                                      leWord64#, ltWord64#, word64ToInt64#)
                                 
+#endif
+#if MIN_VERSION_base(4,14,0)
 #else
 import           GHC.IntWord64 as Compat (wordToWord64#, word64ToWord#,
                                           intToInt64#, int64ToInt#,
